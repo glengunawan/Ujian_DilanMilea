e8c52771217d3672a4333335ca3ce4e2c22222a2
glengunawan <glen.putranto@gmail.com>
2020-02-14 14:27:48 +0700

    Ujian Nomor 3 JC Data Science

--- 3.py
+++ 3.py
@@ -31,7 +31,6 @@ for i in dataProvinsi[str(kodeProvinsiDilan)]:
             if i["city"] == "TANGERANG": 
                 kodePosDilan = i["postal_code"]
                 break
-print(kodePosDilan)
 
 for i in dataProvinsi[str(kodeProvinsiMilea)]: 
     if i["urban"] == "CITARUM" : 
@@ -39,14 +38,13 @@ for i in dataProvinsi[str(kodeProvinsiMilea)]:
             if i["city"] == "BANDUNG": 
                 kodePosMilea = i["postal_code"]
                 break
-print(kodePosMilea)
-# apikey = "AkaypVfrop7FofpXt7tATDTwPKmBM3Lx7NaBXlDnUyBAvgLrop78L2s0kkQvu4OJ" 
-# url = f"https://www.zipcodeapi.com/rest/{apikey}/distance.json/{kodePosDilan}/{kodePosMilea}/km " 
+apikey = "AkaypVfrop7FofpXt7tATDTwPKmBM3Lx7NaBXlDnUyBAvgLrop78L2s0kkQvu4OJ" 
+url = f"https://www.zipcodeapi.com/rest/{apikey}/distance.json/{kodePosDilan}/{kodePosMilea}/km " 
 
-# data = requests.get(url)
-# jarakJson = data.json() 
-# jarak = jarakJson["distance"] 
+data = requests.get(url)
+jarakJson = data.json() 
+jarak = jarakJson["distance"] 
 
-# print(f"Kode Pos lokasi Dilan adalah {kodePosDilan}")
-# print(f"Kode Pos lokasi Milea adalah {kodePosMilea}")
-# print(f"Jarak Dilan & Milea adalah {jarak} km")
+print(f"Kode Pos lokasi Dilan adalah {kodePosDilan}")
+print(f"Kode Pos lokasi Milea adalah {kodePosMilea}")
+print(f"Jarak Dilan & Milea adalah {jarak} km")
